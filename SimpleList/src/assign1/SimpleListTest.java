62e3771d7e5c4aa9ad2a43ff2a49813c43e694f5
lizar <lizar@DESKTOP-8O7FJV1>
2020-02-22 11:54:25 -0700

    SImplelist

---
+++ SimpleList/src/assign1/SimpleListTest.java
@@ -0,0 +1,95 @@
+package assign1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleListTest {
+
+	@Test
+	void testSimpleList() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		//fail("Not yet implemented");
+		SimpleList test = new SimpleList();
+
+		int[] expectedlist = new int[3];
+		
+		expectedlist[0] = 3;	//make lists to test with
+		expectedlist[1] = 2;
+		expectedlist[2] = 1;
+		
+		test.add(1);			//fills lists
+		test.add(2);
+		test.add(3);
+		
+		assertEquals(expectedlist[0],test.list[0]);    //tests if variables are the same
+		assertEquals(expectedlist[1],test.list[1]);
+		assertEquals(expectedlist[2],test.list[2]);
+	}
+
+	@Test
+	void testRemove() {
+		//fail("Not yet implemented");
+		SimpleList test = new SimpleList();
+
+		int[] expectedlist = new int[3];
+		expectedlist[0] = 3;	//make lists to test with
+		expectedlist[1] = 1;
+	
+		test.add(1);			//fills lists
+		test.add(2);
+		test.add(3);
+		
+		test.remove(2);        //removes 2
+		
+		assertEquals(expectedlist[0],test.list[0]);    //tests if variables are the same
+		assertEquals(expectedlist[1],test.list[1]);
+	}
+
+	@Test
+	void testCount() {
+		//fail("Not yet implemented");
+		SimpleList test = new SimpleList();
+		
+		assertEquals("Count is zero", 0, test.count()); //checks that count is zero
+		
+		test.add(1);				//this will add to the counter
+		
+		assertEquals("Count is one", 1, test.count()); //checks that count is one
+	}
+
+	@Test
+	void testToString() {
+		//fail("Not yet implemented");
+		SimpleList test = new SimpleList();
+
+		String list= "5 4 3";			//string with 5 4 3 to test
+		
+		test.add(3);       //fills list with variables
+		test.add(4);
+		test.add(5);
+		
+		assertEquals(list,test.toString());    //tests if variables equals the same
+	}
+
+	@Test
+	void testSearch() {
+		//fail("Not yet implemented");
+		SimpleList test = new SimpleList();
+		
+		assertEquals("4 not found",-1,test.search(4));  //4 should not be in list
+		
+		test.add(3);			//adds numbers to list
+		test.add(4);
+		test.add(5);
+		
+		assertEquals("4 found in position 1",1,test.search(4)); //checks if 4 is in position one
+	}
+}
